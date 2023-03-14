6514800715478708821c3ff17dd7ed42e6ee0bc0
Jayro Gómez <jaxjayro@gmail.com>
2023-03-13 21:07:24 -0700

    Extracted logic to a low level component

--- components/Counter.jsx
+++ components/Counter.jsx
@@ -1,72 +1,11 @@
 import React, { useState } from 'react'
-import { Text, View, StyleSheet, Vibration } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 import CounterDisplay from './CounterDisplay'
 import CounterBotom from './CounterBottom'
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import { GestureHandlerRootView, PanGestureHandler } from 'react-native-gesture-handler'
-import Animated, {
-  useAnimatedGestureHandler,
-  useSharedValue,
-  useAnimatedStyle,
-  withSpring,
-  withRepeat,
-  withTiming,
-  runOnJS,
-  withSequence
-} from 'react-native-reanimated'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 const Counter = ({ counterName = 'Unknow', value = '0' } = {}) => {
   const [count, setCount] = useState(JSON.parse(value))
-  const startingPosition = 0
-  const x = useSharedValue(startingPosition)
-  const y = useSharedValue(startingPosition)
-
-  const animatedStyles = useAnimatedStyle(() => {
-    return {
-      transform: [
-        { translateX: x.value },
-        { translateY: y.value },
-      ]
-    }
-  })
-  const add = () => {
-    Vibration.vibrate(10)
-    const newCount = count + 1
-    AsyncStorage.setItem(counterName, JSON.stringify(newCount))
-      .then(setCount(newCount))
-      .catch(err => console.log(err))
-  }
-  const less = () => {
-    Vibration.vibrate(10)
-    const newCount = count === 0 ? 0 : count - 1
-    AsyncStorage.setItem(counterName, JSON.stringify(newCount))
-      .then(setCount(newCount))
-      .catch(err => console.log(err))
-  }
-
-  const eventHandler = useAnimatedGestureHandler({
-    onStart: (event, ctx) => {
-      ctx.startX = x.value
-      ctx.startY = y.value
-    },
-    onActive: (event, ctx) => {
-      x.value = event.translationX
-      y.value = event.translationY
-    },
-    onEnd: (event, ctx) => {
-      x.value = withSpring(startingPosition)
-      y.value = withSpring(startingPosition)
-      runOnJS(Vibration.vibrate)(10)
-      console.log(x.value)
-      console.log(y.value)
-      if (x.value > 0) {
-        runOnJS(add)()
-      } else if ( x.value < 0) {
-        runOnJS(less)()
-      }
-    }
-  })
-  
 
   if (count === null) {
     return null
@@ -76,12 +15,7 @@ const Counter = ({ counterName = 'Unknow', value = '0' } = {}) => {
     <View style={{ alignItems: 'center', marginVertical: 20 }}>
       <Text style={styles.title}>{counterName.toUpperCase()}</Text>
     </View>
-
-    <PanGestureHandler onGestureEvent={eventHandler}>
-      <Animated.View style={[animatedStyles, styles.animatedContainer]}>
-        <CounterDisplay count={count} />
-      </Animated.View>
-    </PanGestureHandler>
+    <CounterDisplay counterName={counterName} count={count} func={setCount} />
     <CounterBotom counterName={counterName} func={setCount} />
   </GestureHandlerRootView>
 
@@ -89,10 +23,6 @@ const Counter = ({ counterName = 'Unknow', value = '0' } = {}) => {
 }
 
 const styles = StyleSheet.create({
-  animatedContainer: {
-    flex: 1, 
-    alignItems: 'center'
-  },
   container: {
     paddingBottom: 20
   },
--- components/CounterDisplay.jsx
+++ components/CounterDisplay.jsx
@@ -1,14 +1,82 @@
 import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, Vibration } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { PanGestureHandler } from 'react-native-gesture-handler'
+import Animated, {
+  useAnimatedGestureHandler,
+  useSharedValue,
+  useAnimatedStyle,
+  withSpring,
+  withRepeat,
+  withTiming,
+  runOnJS,
+  withSequence
+} from 'react-native-reanimated'
 
-const CounterDisplay = ({ count }) => {
-  
+const CounterDisplay = ({ counterName, func, count }) => {
+  const startingPosition = 0
+  const x = useSharedValue(startingPosition)
+  const y = useSharedValue(startingPosition)
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: x.value },
+        { translateY: y.value },
+      ]
+    }
+  })
+  const add = () => {
+    Vibration.vibrate(10)
+    const newCount = count + 1
+    AsyncStorage.setItem(counterName, JSON.stringify(newCount))
+      .then(func(newCount))
+      .catch(err => console.log(err))
+  }
+  const less = () => {
+    Vibration.vibrate(10)
+    const newCount = count === 0 ? 0 : count - 1
+    AsyncStorage.setItem(counterName, JSON.stringify(newCount))
+      .then(func(newCount))
+      .catch(err => console.log(err))
+  }
+
+  const eventHandler = useAnimatedGestureHandler({
+    onStart: (event, ctx) => {
+      ctx.startX = x.value
+      ctx.startY = y.value
+    },
+    onActive: (event, ctx) => {
+      x.value = event.translationX
+      y.value = event.translationY
+    },
+    onEnd: (event, ctx) => {
+      x.value = withSpring(startingPosition)
+      y.value = withSpring(startingPosition)
+      runOnJS(Vibration.vibrate)(10)
+      console.log(x.value)
+      console.log(y.value)
+      if (x.value > 0) {
+        runOnJS(add)()
+      } else if ( x.value < 0) {
+        runOnJS(less)()
+      }
+    }
+  })
   return <View style={styles.countContainer}>
+    <PanGestureHandler onGestureEvent={eventHandler}>
+      <Animated.View style={[animatedStyles, styles.animatedContainer]}>
         <Text style={styles.counterDisplay}>{count}</Text>
+      </Animated.View>
+    </PanGestureHandler>
   </View>
 }
 
 const styles = StyleSheet.create({
+  animatedContainer: {
+    flex: 1, 
+    alignItems: 'center'
+  },
   countContainer: {
     paddingBottom: 10
   },
