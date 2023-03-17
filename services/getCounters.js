de1b97f3d18e062b5a1be60c032520b92de37210
Jayro Gómez <jaxjayro@gmail.com>
2023-03-17 15:43:32 -0700

    Changing the unique key method, now storing a separete key called ItemCount to retrieve the count

--- services/getCounters.js
+++ services/getCounters.js
@@ -2,6 +2,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export async function getCounters() {
   const keys = await AsyncStorage.getAllKeys()
-  const dataFromStorage = await AsyncStorage.multiGet(keys)
+  const filteredKeys = keys.filter(key => key !== "ItemCount")
+  const dataFromStorage = await AsyncStorage.multiGet(filteredKeys)
   return dataFromStorage
 }
--- services/setCounter.js
+++ services/setCounter.js
@@ -20,13 +20,16 @@ export const setCounter = async (name, value) => {
   if (filteredVal === '') filteredVal = "0"
   try {
     // Count the number of items in the storage in order to give a unique key
-    const key = await (await AsyncStorage.getAllKeys()).length
-    console.log(key)
+    let key = parseInt( await AsyncStorage.getItem('ItemCount')) || 0
     // if empty gets asiganted a value for key and val
     if (counterName === '') {
       counterName = `Counter${key}`
     }
     await AsyncStorage.setItem(JSON.stringify(key), JSON.stringify({ "counterName": counterName, "count": filteredVal }))
+    // incrementing the key to avoid collisions in keys
+    key++
+    // Storgin the new count of items
+    await AsyncStorage.setItem('ItemCount', key.toString())
   } catch (error) {
     console.error({ message: error })
   }
