56deabf1c2ade425157cca91a20910c41ba9a16f
Jayro Gómez <jaxjayro@gmail.com>
2023-03-16 17:51:25 -0700

    Make draggable the Add counter button (+)

--- components/CounterList.jsx
+++ components/CounterList.jsx
@@ -3,16 +3,52 @@ import { Text, View, StyleSheet, FlatList } from 'react-native'
 import Counter from './Counter'
 import { getCounters } from '../services/getCounters'
 import { Link } from 'react-router-native'
+import { PanGestureHandler } from 'react-native-gesture-handler'
+import Animated, {
+  useAnimatedGestureHandler,
+  useSharedValue,
+  useAnimatedStyle,
+  withSpring,
+  runOnJS,
+} from 'react-native-reanimated'
 
 const CounterList = () => {
   const [counters, setCounters] = useState([])
+  const x = useSharedValue(0)
+  const y = useSharedValue(0)
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: x.value },
+        { translateY: y.value },
+      ]
+    }
+  })
 
   useEffect(() => {
     getCounters().then(data => setCounters(data))
   }, [setCounters])
 
-  console.log(counters)
+  const handleGesture = useAnimatedGestureHandler({
+    onStart: (event, ctx) => {
+      ctx.startX = x.value
+      ctx.startY = y.value
+    },
+    onActive: (event, ctx) => {
+      x.value = event.translationX
+      y.value = event.translationY
 
+      if (y.value < -100) y.value = -100
+      if (x.value > 100) x.value = 100
+      if (x.value < -100) x.value = -100
+      
+    },
+    onEnd: (event, ctx) => {
+      x.value = 0
+      y.value = 0
+    }
+  })
   return (<View style={styles.container}>
     <Text style={styles.title}>Miscellaneous List</Text>
     <FlatList
@@ -32,9 +68,13 @@ const CounterList = () => {
         )
       }}
     />
-    <Link to='/new' underlayColor='transparent'>
-      <Text style={styles.new}> + </Text>
-    </Link>
+    <PanGestureHandler onGestureEvent={handleGesture}>
+      <Animated.View style={animatedStyles}>
+        <Link to='/new' underlayColor='transparent'>
+          <Text style={styles.new}> + </Text>
+        </Link>
+      </Animated.View>
+    </PanGestureHandler>
   </View>
   )
 }
@@ -46,13 +86,16 @@ const styles = StyleSheet.create({
   },
   title: {
     textAlign: 'center',
-    fontSize: 40,
-    backgroundColor: 'rgba(193, 201, 205, 0.7)',
-    color: '#fff',
-    borderRadius: 10,
-    paddingHorizontal: 30,
-    paddingVertical: 10,
-    marginBottom: 30,
+    fontSize: 48,
+    color: '#F5F5F5',
+    borderRadius: 8,
+    paddingHorizontal: 24,
+    paddingTop: 11,
+    marginBottom: 24,
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 4,
   },
   new: {
     fontSize: 60,
