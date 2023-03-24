dbad2ad3f207efc8b82dbacb48f32d6eea188020
Jayro Gómez <jaxjayro@gmail.com>
2023-03-23 21:36:46 -0700

    Modified the animatedStyles to work with or without the y shared value

--- utils/animatedStyles.js
+++ utils/animatedStyles.js
@@ -1,12 +1,17 @@
 import { useAnimatedStyle } from 'react-native-reanimated'
 
 export const animatedMove = (x,y) => {
+ 
   return useAnimatedStyle(() => {
-    return {
+    return y && x ? {
       transform: [
         { translateX: x.value },
         { translateY: y.value },
       ]
-    }
+    } : x ? {
+      transform: [
+        { translateX: x.value },
+      ]
+    } : null
   })
 }
